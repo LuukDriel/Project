7c0dae00d0c3e34e6664741f20d3622d5f381a21
LuukDriel <166855482+LuukDriel@users.noreply.github.com>
2025-03-14 10:00:02 +0100

    game gefixt

--- Vier op een rij/JS/vieropeenrij.js
+++ Vier op een rij/JS/vieropeenrij.js
@@ -22,9 +22,8 @@ function setGame() {
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
-            
             row.push(' ');
-            
+
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
@@ -77,23 +76,23 @@ function setPiece() {
 }
 
 function checkWinner() {
-     // horizontaal
-     for (let r = 0; r < rows; r++) {
-         for (let c = 0; c < columns - 3; c++){
+    // horizontaal
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                if (board[r][c] == board[r][c + 1] && board[r][c + 1] == board[r][c + 2] && board[r][c + 2] == board[r][c + 3]) {
                     setWinner(r, c);
                     return;
                 }
             }
-         }
+        }
     }
 
     // verticaal
     for (let c = 0; c < columns; c++) {
         for (let r = 0; r < rows - 3; r++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                if (board[r][c] == board[r + 1][c] && board[r + 1][c] == board[r + 2][c] && board[r + 2][c] == board[r + 3][c]) {
                     setWinner(r, c);
                     return;
                 }
@@ -105,7 +104,7 @@ function checkWinner() {
     for (let r = 0; r < rows - 3; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                if (board[r][c] == board[r + 1][c + 1] && board[r + 1][c + 1] == board[r + 2][c + 2] && board[r + 2][c + 2] == board[r + 3][c + 3]) {
                     setWinner(r, c);
                     return;
                 }
@@ -117,7 +116,7 @@ function checkWinner() {
     for (let r = 3; r < rows; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                if (board[r][c] == board[r - 1][c + 1] && board[r - 1][c + 1] == board[r - 2][c + 2] && board[r - 2][c + 2] == board[r - 3][c + 3]) {
                     setWinner(r, c);
                     return;
                 }
@@ -134,4 +133,9 @@ function setWinner(r, c) {
         winner.innerText = "Yellow Wins";
     }
     gameOver = true;
+}
+
+// Define the triggerPowerUp function
+function triggerPowerUp() {
+    // Implement power-up logic here
 }
